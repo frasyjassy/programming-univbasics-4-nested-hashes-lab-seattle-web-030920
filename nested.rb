df2c4bb73449cd2140a7b706b2716eea8314d3e0
JC Ramirez <jsmnramirez25@gmail.com>
2020-04-09 18:59:39 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,6 +16,9 @@ def hopper
       }
     }
 
+programmer_hash [:grace_hopper] 
+
+
 end
 
 def alan_kay_is_known_for
@@ -36,6 +39,8 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
+    
+programmer_hash [:alan_kay] [:known_for] = "Object Orientation"
 
 end
 
@@ -55,6 +60,8 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+    
+programmer_hash [:dennis_ritchie] [:languages] = "C"
 
 end
 
@@ -68,6 +75,33 @@ def adding_matz
 
 	programmer_hash = 
  		{
+ 		  :yukihiro_matsumoto => {
+      :known_for => "Ruby",
+      :languages => ["LISP", "C"]
+      },
+ 		  
+      :grace_hopper => {
+        :known_for => "COBOL",
+        :languages => ["COBOL", "FORTRAN"]
+      },
+      :alan_kay => {
+        :known_for => "Object Orientation",
+        :languages => ["Smalltalk", "LISP"]
+      },
+      :dennis_ritchie => {
+        :known_for => "Unix",
+        :languages => ["C"]
+      }
+    }
+    
+programmer_hash = 
+
+    {
+ 		  :yukihiro_matsumoto => {
+      :known_for => "Ruby",
+      :languages => ["LISP", "C"]
+      },
+ 		  
       :grace_hopper => {
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
@@ -103,6 +137,24 @@ def changing_alan
         :languages => ["C"]
       }
     }
+    
+programmer_hash [:alan_kay] [:known_for] = "GUI"
+
+programmer_hash = 
+ 		{
+      :grace_hopper => {
+        :known_for => "COBOL",
+        :languages => ["COBOL", "FORTRAN"]
+      },
+      :alan_kay => {
+        :known_for => "GUI",
+        :languages => ["Smalltalk", "LISP"]
+      },
+      :dennis_ritchie => {
+        :known_for => "Unix",
+        :languages => ["C"]
+      }
+    }
 
 end
 
@@ -123,6 +175,24 @@ def adding_to_dennis
       :dennis_ritchie => {
         :known_for => "Unix",
         :languages => ["C"]
+      }
+    }
+
+programmer_hash [:dennis_ritchie] [:languages] = "Assembly"
+
+	programmer_hash = 
+ 		{
+      :grace_hopper => {
+        :known_for => "COBOL",
+        :languages => ["COBOL", "FORTRAN"]
+      },
+      :alan_kay => {
+        :known_for => "Object Orientation",
+        :languages => ["Smalltalk", "LISP"]
+      },
+      :dennis_ritchie => {
+        :known_for => "Unix",
+        :languages => ["C","Assembly"]
       }
     }
 
