9099a8a0334a7885cb9a7e344575a7c9daa9f12f
JC Ramirez <jsmnramirez25@gmail.com>
2020-04-09 18:47:02 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -93,6 +93,28 @@ def adding_matz
         :languages => ["C"]
       }
     }
+    
+programmer_hash = 
+
+{
+ 		  :yukihiro_matsumoto => {
+      :known_for => "Ruby",
+      :languages => ["LISP", "C"]
+      },
+ 		  
+      :grace_hopper => {
+        :known_for => "COBOL",
+        :languages => ["COBOL", "FORTRAN"]
+      },
+      :alan_kay => {
+        :known_for => "Object Orientation",
+        :languages => ["Smalltalk", "LISP"]
+      },
+      :dennis_ritchie => {
+        :known_for => "Unix",
+        :languages => ["C"]
+      }
+    }
 
 end
 
