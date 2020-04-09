267c919d8014872814c249197f65e87147c5d075
JC Ramirez <jsmnramirez25@gmail.com>
2020-04-09 18:59:09 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -180,6 +180,20 @@ def adding_to_dennis
 
 programmer_hash [:dennis_ritchie] [:languages] = "Assembly"
 
-
+	programmer_hash = 
+ 		{
+      :grace_hopper => {
+        :known_for => "COBOL",
+        :languages => ["COBOL", "FORTRAN"]
+      },
+      :alan_kay => {
+        :known_for => "Object Orientation",
+        :languages => ["Smalltalk", "LISP"]
+      },
+      :dennis_ritchie => {
+        :known_for => "Unix",
+        :languages => ["C","Assembly"]
+      }
+    }
 
 end
